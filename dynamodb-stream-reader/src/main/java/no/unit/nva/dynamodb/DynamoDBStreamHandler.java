06ec8c3a791f4e5f360c51b50e20b274c446dc9b
svenngjeto <svenn.gjeto@unit.no>
2020-08-13 16:40:54 +0200

    Using DynamodbEvent as input

--- dynamodb-stream-reader/src/main/java/no/unit/nva/dynamodb/DynamoDBStreamHandler.java
+++ dynamodb-stream-reader/src/main/java/no/unit/nva/dynamodb/DynamoDBStreamHandler.java
@@ -2,13 +2,14 @@ package no.unit.nva.dynamodb;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import nva.commons.utils.JacocoGenerated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-public class DynamoDBStreamHandler implements RequestHandler<Map<String,Object>, Map<String,Object>> {
+public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, Map<String,Object>> {
 
         private static final Logger logger = LoggerFactory.getLogger(DynamoDBStreamHandler.class);
 
@@ -21,10 +22,10 @@ public class DynamoDBStreamHandler implements RequestHandler<Map<String,Object>,
     }
 
     @Override
-    public Map<String,Object> handleRequest(Map<String,Object> event, Context context) {
-        System.out.println("event: "+ event);
+    public Map<String,Object> handleRequest(DynamodbEvent event, Context context) {
+        System.out.println("DynamodbEvent: "+ event);
         logger.debug("event: {}",event);
-        return event;
+        return Map.of("event", event);
     }
 
 }
