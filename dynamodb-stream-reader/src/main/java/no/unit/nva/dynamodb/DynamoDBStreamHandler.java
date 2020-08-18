0c840f9ce9571de8a9aa790f68144165fd116785
Svenn <svenn.gjeto@gmail.com>
2020-08-18 16:24:42 +0200

    more Dead code cleanup, added tests

--- dynamodb-stream-reader/src/main/java/no/unit/nva/dynamodb/DynamoDBStreamHandler.java
+++ dynamodb-stream-reader/src/main/java/no/unit/nva/dynamodb/DynamoDBStreamHandler.java
@@ -4,51 +4,19 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import elasticsearch.ElasticSearchRestClient;
-import no.unit.nva.model.Publication;
 import nva.commons.utils.JacocoGenerated;
-import nva.commons.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, String> {
 
+    public static final String IDENTIFIER = "identifier";
+
     private static final Logger logger = LoggerFactory.getLogger(DynamoDBStreamHandler.class);
-    private final ObjectMapper objectMapper = JsonUtils.objectMapper;
     private final ElasticSearchRestClient elasticSearchClient = new ElasticSearchRestClient();
-
-    private final Function<DynamodbEvent.DynamodbStreamRecord, String> getIdentifier =
-            (DynamodbEvent.DynamodbStreamRecord streamRecord) -> {
-                Map<String, AttributeValue> keys = streamRecord.getDynamodb().getKeys();
-                AttributeValue identifierAttribute = keys.get("identifier");
-                String identifier = identifierAttribute.getS();
-                logger.trace("extracted identifier: {}", identifier);
-                return identifier;
-            };
-
-    private void flatten(PublicationIndexDocument target, String prefix, Map<String, AttributeValue> valueMap) {
-        logger.debug("flatten: {}", valueMap);
-        valueMap.forEach((k, v) -> {
-            if (v.getM() == null) {
-                target.putIndexValue(addIndexPrefix(prefix,k), v.getS());
-            } else {
-                flatten(target,k, v.getM());
-            }
-        });
-    }
-
-    private String addIndexPrefix(String prefix, String k) {
-        if (prefix != null && !prefix.isEmpty()) {
-            return prefix+"."+k;
-        } else {
-            return k;
-        }
-    };
 
     /**
      * Default constructor for DynamoDBStreamHandler.
@@ -57,7 +25,6 @@ public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, Stri
     public DynamoDBStreamHandler() {
 
     }
-
 
     @Override
     public String handleRequest(DynamodbEvent event, Context context) {
@@ -78,7 +45,7 @@ public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, Stri
     }
 
     private void upsertSearchIndex(DynamodbEvent.DynamodbStreamRecord streamRecord) {
-        String identifier = streamRecord.getDynamodb().getKeys().get("identifier").getS();
+        String identifier = streamRecord.getDynamodb().getKeys().get(IDENTIFIER).getS();
         Map<String, AttributeValue> valueMap = streamRecord.getDynamodb().getNewImage();
         PublicationIndexDocument flattenedPublication = new PublicationIndexDocument(identifier);
         flatten(flattenedPublication, "", valueMap);
@@ -87,10 +54,28 @@ public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, Stri
     }
 
     private void removeFromSearchIndex(DynamodbEvent.DynamodbStreamRecord streamRecord) {
-        String identifier = getIdentifier.apply(streamRecord);
+        String identifier = streamRecord.getDynamodb().getKeys().get(IDENTIFIER).getS();
         logger.trace("Deleting from search API publication with identifier: {}", identifier);
         elasticSearchClient.removeDocumentFromIndex(identifier);
     }
 
+    private void flatten(PublicationIndexDocument target, String prefix, Map<String, AttributeValue> valueMap) {
+        logger.debug("flatten: {}", valueMap);
+        valueMap.forEach((k, v) -> {
+            if (v.getM() == null) {
+                target.putIndexValue(addIndexPrefix(prefix,k), v.getS());
+            } else {
+                flatten(target,k, v.getM());
+            }
+        });
+    }
+
+    private String addIndexPrefix(String prefix, String k) {
+        if (prefix != null && !prefix.isEmpty()) {
+            return prefix + "." + k;
+        } else {
+            return k;
+        }
+    };
 
 }
--- dynamodb-stream-reader/src/main/java/no/unit/nva/dynamodb/PublicationIndexDocument.java
+++ dynamodb-stream-reader/src/main/java/no/unit/nva/dynamodb/PublicationIndexDocument.java
@@ -1,8 +1,5 @@
 package no.unit.nva.dynamodb;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +7,6 @@ public class PublicationIndexDocument {
 
     private final String identifier;
     private final Map<String, String> indexValues = new HashMap<>();
-    private static final Logger logger = LoggerFactory.getLogger(DynamoDBStreamHandler.class);
 
 
     public PublicationIndexDocument(String identifier) {
@@ -25,7 +21,6 @@ public class PublicationIndexDocument {
     public String toString() {
         return "FlattenedPublicationIndexRecord{" +
                 "identifier='" + identifier + '\'' +
-                ", indexValues=" + indexValues +
-                '}';
+                ", indexValues=" + indexValues + '}';
     }
 }
