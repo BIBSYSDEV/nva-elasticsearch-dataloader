4fd991af93e01cc31b2bffda955d45c95c436445
svenngjeto <svenn.gjeto@unit.no>
2020-08-27 13:28:59 +0200

    Added new document class to hold values for upload to elasticsearch

---
+++ dynamodb-stream-reader/src/main/java/elasticsearch/ElasticSearchIndexDocument.java
@@ -0,0 +1,104 @@
+package elasticsearch;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import nva.commons.utils.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ElasticSearchIndexDocument {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticSearchIndexDocument.class);
+
+    private static final String CONTRIBUTORS_KEY = "contributor";
+    private static final String TITLE_KEY = "title";
+    private static final String DATE_KEY = "date";
+    private static final String RESOURCE_TYPE_KEY = "type";
+    public static final String ID_KEY = "id";
+    public static final String EMPTY_STRING = "";
+
+    private final String internalIdentifier;
+
+    private final String serviceUrl;
+    private final String indexName;
+    private final Map<String, Object> values = new HashMap<>();
+
+    /**
+     * Created an document suitable to upload to elasticsearch.
+     * @param indexName which elasticsearch index handles this document
+     * @param serviceUrl prefix of serviceurl for creating URI for resource source
+     * @param internalIdentifier shor internal identifier (uuid) for resource
+     */
+    public ElasticSearchIndexDocument(String indexName, String serviceUrl,  String internalIdentifier) {
+        this.indexName = indexName;
+        this.serviceUrl = serviceUrl;
+        this.internalIdentifier = internalIdentifier;
+        values.put(ID_KEY, serviceUrl + internalIdentifier);
+    }
+
+    public String getServiceUrl() {
+        return serviceUrl;
+    }
+
+    public String getIndexName() {
+        return indexName;
+    }
+
+    public String getInternalIdentifier() {
+        return internalIdentifier;
+    }
+
+    /**
+     * Adds a contributors name to the document.
+     * The document an hold multiple contributors
+     * @param name name of the contributor
+     */
+    public void addContributor(String name) {
+        Set<String> contributors = (Set<String>) values.computeIfAbsent(CONTRIBUTORS_KEY, l ->  new HashSet<String>());
+        contributors.add(name);
+        values.put(CONTRIBUTORS_KEY,contributors);
+    }
+
+    /**
+     * Sets the title of the resource in document.
+     * @param title title of resource
+     */
+    public void setTitle(String title) {
+        values.put(TITLE_KEY, title);
+    }
+
+    /**
+     * Date assosiated with the resource.
+     * @param dateString date on the form "YYYY-MM-DD"
+     */
+    public void setDate(String dateString) {
+        values.put(DATE_KEY, dateString);
+    }
+
+    /**
+     * Sets the type of the resource.
+     * @param resourceType type of resource. ie. JournalArticle
+     */
+    public void setResourceType(String resourceType) {
+        values.put(RESOURCE_TYPE_KEY, resourceType);
+    }
+
+    /**
+     * Generates JSON string to be inserted into elasticsearch.
+     * @return string representation of the document
+     */
+    public String toJson() {
+        try {
+            return JsonUtils.objectMapper.writeValueAsString(values);
+        } catch (JsonProcessingException e) {
+            logger.error(EMPTY_STRING,e);
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ dynamodb-stream-reader/src/test/java/no/unit/nva/elasticsearch/ElasticSearchIndexDocumentTest.java
@@ -0,0 +1,56 @@
+package no.unit.nva.elasticsearch;
+
+import elasticsearch.ElasticSearchIndexDocument;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ElasticSearchIndexDocumentTest {
+
+    private static final String INDEX_NAME = "someIndex";
+    private static final String SERVICE_URL = "http://localhost/";
+    private static final String INTERNAL_IDENTIFIER = "id";
+    public static final String SAMPLE_CONTRIBUTOR = "Doe, John";
+    public static final String SAMPLE_DATE = "2020-08-26";
+    public static final String SAMPLE_RESOURCETYPE = "";
+    public static final String SAMPLE_TITLE = "This Is A Sample Title";
+
+    @Test
+    @DisplayName("Testing ElasticSearchIndexDocument constructor and initializing")
+    public void testElasticSearchIndexDocumentConstructor() {
+        ElasticSearchIndexDocument document =
+                new ElasticSearchIndexDocument(INDEX_NAME, SERVICE_URL, INTERNAL_IDENTIFIER);
+        assertNotNull(document);
+        assertEquals(INDEX_NAME, document.getIndexName());
+        assertEquals(SERVICE_URL, document.getServiceUrl());
+        assertEquals(INTERNAL_IDENTIFIER, document.getInternalIdentifier());
+        assertTrue(document.toJson().contains(ElasticSearchIndexDocument.ID_KEY));
+    }
+
+    @Test
+    @DisplayName("Testing ElasticSearchIndexDocument constructor, minimum values added")
+    public void testElasticSearchIndexDocumentConstructorAndCompleteAssignment() {
+        ElasticSearchIndexDocument document =
+                new ElasticSearchIndexDocument(INDEX_NAME, SERVICE_URL, INTERNAL_IDENTIFIER);
+        assertNotNull(document);
+
+
+        document.setResourceType(SAMPLE_RESOURCETYPE);
+        document.setDate(SAMPLE_DATE);
+        document.addContributor(SAMPLE_CONTRIBUTOR);
+        document.setTitle(SAMPLE_TITLE);
+
+        String json = document.toJson();
+
+        assertTrue(json.contains(ElasticSearchIndexDocument.ID_KEY));
+        assertTrue(json.contains(SAMPLE_DATE));
+        assertTrue(json.contains(SAMPLE_RESOURCETYPE));
+        assertTrue(json.contains(SAMPLE_CONTRIBUTOR));
+        assertTrue(json.contains(SAMPLE_TITLE));
+
+    }
+
+}
