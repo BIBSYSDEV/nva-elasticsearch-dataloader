97ef49ed757a22fbe8cf10081037217791ff524b
svenngjeto <svenn.gjeto@unit.no>
2020-08-31 15:45:51 +0200

    Added counting to tests

--- dynamodb-stream-reader/src/test/java/no/unit/nva/dynamodb/DynamoDBStreamHandlerTest.java
+++ dynamodb-stream-reader/src/test/java/no/unit/nva/dynamodb/DynamoDBStreamHandlerTest.java
@@ -18,11 +18,15 @@ import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 @SuppressWarnings("unchecked")
 public class DynamoDBStreamHandlerTest {
@@ -38,7 +42,6 @@ public class DynamoDBStreamHandlerTest {
 
     private DynamoDBStreamHandler handler;
     private ElasticSearchRestClient elasticSearchRestClient;
-    private Environment environment;
     private Context context;
     private HttpClient httpClient;
 
@@ -50,9 +53,18 @@ public class DynamoDBStreamHandlerTest {
      */
     @BeforeEach
     public void init() throws IOException, InterruptedException {
-        environment = spy(Environment.class);
         context = mock(Context.class);
         httpClient = spy(HttpClient.class);
+        Environment environment = setupMockEnvironment();
+
+        HttpResponse<String> successResponse = mock(HttpResponse.class);
+        doReturn(successResponse).when(httpClient).send(any(), any());
+        elasticSearchRestClient = new ElasticSearchRestClient(httpClient, environment);
+        handler = new DynamoDBStreamHandler(elasticSearchRestClient, environment);
+    }
+
+    private Environment setupMockEnvironment() {
+        Environment environment = mock(Environment.class);
         doReturn(ELASTICSEARCH_ENDPOINT_ADDRESS).when(environment)
                 .readEnv(ElasticSearchRestClient.ELASTICSEARCH_ENDPOINT_ADDRESS_KEY);
         doReturn(ELASTICSEARCH_ENDPOINT_INDEX).when(environment)
@@ -64,51 +76,53 @@ public class DynamoDBStreamHandlerTest {
                 .readEnv(DynamoDBStreamHandler.TARGET_SERVICE_URL_KEY);
         doReturn(ELASTICSEARCH_ENDPOINT_INDEX).when(environment)
                 .readEnv(DynamoDBStreamHandler.ELASTICSEARCH_ENDPOINT_INDEX_KEY);
-
-        HttpResponse<String> successResponse = mock(HttpResponse.class);
-        doReturn(successResponse).when(httpClient).send(any(), any());
-        elasticSearchRestClient = new ElasticSearchRestClient(httpClient, environment);
-        handler = new DynamoDBStreamHandler(elasticSearchRestClient, environment);
+        return environment;
     }
 
     @Test
     @DisplayName("testCreateHandlerWithEmptyEnvironmentShouldFail")
-    public void testCreateHandlerWithEmptyEnvironmentShouldFail()  {
-        assertThrows(IllegalStateException.class, () -> new DynamoDBStreamHandler());
+    public void testCreateHandlerWithEmptyEnvironmentShouldFail() throws IOException, InterruptedException {
+        Exception exception  = assertThrows(IllegalStateException.class, () -> new DynamoDBStreamHandler());
+        assertTrue(exception.getMessage().contains("Environment variable not set"));
+        verify(httpClient,atMost(0)).send(any(), any());
     }
-
-
 
     @Test
     @DisplayName("testHandlerHandleSimpleModifyEventWithoutProblem")
-    public void testHandlerHandleSimpleModifyEventWithoutProblem() throws IOException {
+    public void testHandlerHandleSimpleModifyEventWithoutProblem() throws IOException, InterruptedException {
         DynamodbEvent requestEvent = loadEventFromResourceFile(SAMPLE_MODIFY_EVENT_FILENAME);
         String response =  handler.handleRequest(requestEvent, context);
+        verify(httpClient,atLeast(1)).send(any(), any());
+        verify(httpClient,atMost(1)).send(any(), any());
         assertNotNull(response);
     }
 
     @Test
     @DisplayName("testHandlerHandleSimpleInsertEventWithoutProblem")
-    public void testHandlerHandleSimpleInsertEventWithoutProblem() throws IOException {
+    public void testHandlerHandleSimpleInsertEventWithoutProblem() throws IOException, InterruptedException {
         DynamodbEvent requestEvent = loadEventFromResourceFile(SAMPLE_INSERT_EVENT_FILENAME);
         String response =  handler.handleRequest(requestEvent, context);
+        verify(httpClient,atLeast(1)).send(any(), any());
+        verify(httpClient,atMost(1)).send(any(), any());
         assertNotNull(response);
     }
 
-
     @Test
     @DisplayName("testHandlerHandleSimpleRemoveEventWithoutProblem")
-    public void testHandlerHandleSimpleRemoveEventWithoutProblem() throws IOException {
+    public void testHandlerHandleSimpleRemoveEventWithoutProblem() throws IOException, InterruptedException {
         DynamodbEvent requestEvent = loadEventFromResourceFile(SAMPLE_REMOVE_EVENT_FILENAME);
         String response =  handler.handleRequest(requestEvent, context);
+        verify(httpClient,atLeast(1)).send(any(), any());
+        verify(httpClient,atMost(1)).send(any(), any());
         assertNotNull(response);
     }
 
     @Test
     @DisplayName("testHandleUnknownEventToGiveException")
-    public void testHandleUnknownEventToGiveException() throws IOException {
+    public void testHandleUnknownEventToGiveException() throws IOException, InterruptedException {
         DynamodbEvent requestEvent = loadEventFromResourceFile(SAMPLE_UNKNOWN_EVENT_FILENAME);
         assertThrows(RuntimeException.class, () -> handler.handleRequest(requestEvent, context));
+        verify(httpClient,atMost(0)).send(any(), any());
     }
 
     @Test
@@ -117,12 +131,13 @@ public class DynamoDBStreamHandlerTest {
         DynamodbEvent requestEvent = loadEventFromResourceFile(SAMPLE_MODIFY_EVENT_FILENAME);
         doThrow(IOException.class).when(httpClient).send(any(), any());
         assertThrows(RuntimeException.class, () -> handler.handleRequest(requestEvent, context));
+        verify(httpClient,atMost(1)).send(any(), any());
+        verify(httpClient,atLeast(1)).send(any(), any());
     }
 
     private DynamodbEvent loadEventFromResourceFile(String filename) throws IOException {
         InputStream is = IoUtils.inputStreamFromResources(Paths.get(filename));
         return JsonUtils.objectMapper.readValue(is, DynamodbEvent.class);
     }
-
 
 }
